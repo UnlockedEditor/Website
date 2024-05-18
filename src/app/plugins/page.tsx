db0e88d405268bf515b86502158440ca3eec7d2d
VeygaX <84923630+veygax@users.noreply.github.com>
2024-05-18 14:28:32 +0100

    added plugins page

---
+++ src/app/plugins/page.tsx
@@ -0,0 +1,9 @@
+import Plugins from "@/components/plugins/plugins";
+
+export default function PluginPage() {
+  return (
+    <main className="flex h-auto flex-col items-center justify-between overflow-hidden">
+      <Plugins />
+    </main>
+  );
+}
