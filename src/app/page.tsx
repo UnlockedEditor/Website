ea218c7ad31721aa86c608fa0eecc391642bcc2a
VeygaX <84923630+veygax@users.noreply.github.com>
2024-06-03 21:01:34 +0100

    improved marketplace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Home from "@/components/home/home";
 
 export default function MainPage() {
   return (
-    <main className="flex h-auto flex-col items-center justify-between p-24 overflow-hidden">
+    <main className="flex h-auto flex-col items-center justify-between overflow-hidden min-w-screen min-h-screen">
       <Home />
     </main>
   );
--- src/app/plugins/page.tsx
+++ src/app/plugins/page.tsx
@@ -2,7 +2,7 @@ import Plugins from "@/components/plugins/plugins";
 
 export default function PluginPage() {
   return (
-    <main className="flex h-auto flex-col items-center justify-between overflow-hidden">
+    <main className="flex h-auto flex-col items-center justify-between overflow-hidden min-w-screen min-h-screen">
       <Plugins />
     </main>
   );
